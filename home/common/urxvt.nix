64648ad8687887c367526842e3b732e68173b891
c1d3r24 <shadow.cyph3r@pm.me>
2024-07-03 21:50:43 -0700

    added urxvt conf for opi

--- home/common/urxvt.nix
+++ home/common/urxvt.nix
@@ -3,51 +3,69 @@
 {
   programs.urxvt = {
     enable = true;
-    font = "xft:Monospace:size=10";
-    scrollBar = false;
-    scrollBar_right = false;
-    depth = 24;
-    borderLess = true;
-    lineSpacing = 0;
-    letterSpace = 0;
-    geometry = "92x24";
-    internalBorder = 10;
-    cursorBlink = true;
-    cursorUnderline = false;
-    saveLine = 2048;
-    urgentOnBell = true;
+    fonts = [ "xft:Monospace:size=10" ];
     iso14755 = false;
-
-    # Colors
-    foreground = "#c5c8c6";
-    background = "#1d1f21";
-    cursorColor = "#c5c8c6";
-    colors = {
-      black   = "#282a2e";
-      red     = "#a54242";
-      green   = "#8c9440";
-      yellow  = "#de935f";
-      blue    = "#5f819d";
-      magenta = "#85678f";
-      cyan    = "#5e8d87";
-      white   = "#707880";
-
-      brightBlack   = "#373b41";
-      brightRed     = "#cc6666";
-      brightGreen   = "#b5bd68";
-      brightYellow  = "#f0c674";
-      brightBlue    = "#81a2be";
-      brightMagenta = "#b294bb";
-      brightCyan    = "#8abeb7";
-      brightWhite   = "#c5c8c6";
+    scroll = {
+      bar = {
+        enable = false;
+        position = "right";
+        align = "bottom";
+        floating = false;
+        style = "plain";
+      };
+      keepPosition = true;
+      lines = 2048;
+      scrollOnKeystroke = true;
+      scrollOnOutput = true;
     };
-
-    # Transparency settings
     transparent = true;
     shading = 30;
+    extraConfig = ''
+      URxvt*foreground: #c5c8c6
+      URxvt*background: #1d1f21
+      URxvt*cursorColor: #c5c8c6
 
-    # Perl extensions
-    perlExtCommon = "config-reload,font-size";
+      ! black
+      URxvt*color0: #282a2e
+      URxvt*color8: #373b41
+
+      ! red
+      URxvt*color1: #a54242
+      URxvt*color9: #cc6666
+
+      ! green
+      URxvt*color2: #8c9440
+      URxvt*color10: #b5bd68
+
+      ! yellow
+      URxvt*color3: #de935f
+      URxvt*color11: #f0c674
+
+      ! blue
+      URxvt*color4: #5f819d
+      URxvt*color12: #81a2be
+
+      ! magenta
+      URxvt*color5: #85678f
+      URxvt*color13: #b294bb
+
+      ! cyan
+      URxvt*color6: #5e8d87
+      URxvt*color14: #8abeb7
+
+      ! white
+      URxvt*color7: #707880
+      URxvt*color15: #c5c8c6
+
+      URxvt.perl-ext-common: config-reload,font-size
+      URxvt.letterSpace: 0
+      URxvt.lineSpace: 0
+      URxvt.geometry: 92x24
+      URxvt.internalBorder: 10
+      URxvt.cursorBlink: true
+      URxvt.cursorUnderline: false
+      URxvt.urgentOnBell: true
+    '';
   };
 
   # Optionally, additional configurations
