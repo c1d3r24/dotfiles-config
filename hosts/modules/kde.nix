f62421f3c0f418f8a2fe51402287ce110318d0e1
c1d3r24 <shadow.cyph3r@pm.me>
2024-09-17 21:05:28 -0700

    switched to kde

---
+++ hosts/modules/kde.nix
@@ -0,0 +1,12 @@
+
+{ config, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.xkb.layout = "us";
+
+  # Enable the KDE Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  programs.kdeconnect.enable = true;
+}
