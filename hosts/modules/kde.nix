d3fd049b009321012284b39e4a318799a0740f6f
c1d3r24 <shadow.cyph3r@pm.me>
2024-09-17 21:14:29 -0700

    kde needs x11

--- hosts/modules/kde.nix
+++ hosts/modules/kde.nix
@@ -6,6 +6,7 @@
   services.xserver.xkb.layout = "us";
 
   # Enable the KDE Desktop Environment.
+  services.displayManager.defaultSession = "plasmax11";
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   programs.kdeconnect.enable = true;
